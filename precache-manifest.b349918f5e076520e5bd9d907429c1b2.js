f3ae408d2d9feefbbf93db9806013e7767e138e1
cmcdermitt <charliemc1996@gmail.com>
2019-12-28 14:09:51 -0500

    Updates

---
+++ precache-manifest.b349918f5e076520e5bd9d907429c1b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d9658e3e920178445c92b2c55c43aa",
+    "url": "/catchphrase-ba/index.html"
+  },
+  {
+    "revision": "873fda7662a7896cd810",
+    "url": "/catchphrase-ba/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7f2eca45089edf96b941",
+    "url": "/catchphrase-ba/static/js/2.24a0d4b5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/catchphrase-ba/static/js/2.24a0d4b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "873fda7662a7896cd810",
+    "url": "/catchphrase-ba/static/js/main.03ede11c.chunk.js"
+  },
+  {
+    "revision": "dedb3ff6e68f19c42264",
+    "url": "/catchphrase-ba/static/js/runtime-main.8c40e88f.js"
+  },
+  {
+    "revision": "535a4cf54d6414c06c6101b427306867",
+    "url": "/catchphrase-ba/static/media/buzzer.535a4cf5.mp3"
+  }
+]);
